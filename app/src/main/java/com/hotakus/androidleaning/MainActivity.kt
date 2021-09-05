039821998776eabfc17782d59fce644f0f21ebfc
Hotakus <553070703@qq.com>
2021-09-05 17:44:29 +0800

    learning

--- app/src/main/java/com/hotakus/androidleaning/MainActivity.kt
+++ app/src/main/java/com/hotakus/androidleaning/MainActivity.kt
@@ -2,10 +2,22 @@ package com.hotakus.androidleaning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hotakus.androidleaning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private fun bindingInit() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingInit()
+        setContentView(binding.root)
+
+        binding.t.text = "test"
+
     }
 }
